97c0a917fc0cb8aab01765db38598f8c431d8ba5
CraftingTable0 <82964449+CraftingTable0@users.noreply.github.com>
2022-01-07 18:31:54 +0300

    ilk push için hazır

--- Türk_Arama_Motoru_Tarık/motorapp/urls.py
+++ Türk_Arama_Motoru_Tarık/motorapp/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.Home_Page),
-    path("newview", views.New)
 ]
--- Türk_Arama_Motoru_Tarık/motorapp/views.py
+++ Türk_Arama_Motoru_Tarık/motorapp/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def Home_Page(request):
     return render(request, "content/index.html")
-
-def New(request):
-    return render(request, "content/new.html")
